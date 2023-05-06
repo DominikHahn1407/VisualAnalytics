3558068810bce655496009913e7545e364a6ca8a
Dominik Hahn <dominik.hahn@hahnelektro.de>
2023-05-06 21:40:21 +0200

    updated dashboard

--- dashboard.py
+++ dashboard.py
@@ -1,5 +1,6 @@
 import os
-# import streamlit as st
+import pickle
+import streamlit as st
 import numpy as np
 
 from PIL import Image
@@ -7,16 +8,21 @@ from keras.models import load_model
 from keras.applications.inception_v3 import preprocess_input
 
 
-TEST_DIR = os.path.join(os.getcwd(), "data/test")
+with open('le_classes.pkl', 'rb') as f:
+    classes = pickle.load(f)
+
+IMG_PATH = os.path.join(os.getcwd(), "data/images/3Blue1Brown/3d6DsjIBzJ4.jpg")
 TARGET_SIZE = (224, 224)
 
 model = load_model("./Inception_0933.h5")
-dir = os.listdir(TEST_DIR)
-rand_int = np.random.randint(len(dir))
-image_path = os.path.join(TEST_DIR, f"test_{rand_int}.png")
 
-img = Image.open(image_path)
-img = np.array(img, dtype=np.uint8)
-img_processed = preprocess_input(img)
+img = Image.open(IMG_PATH)
+rgb_image = img.copy().convert("RGB")
+img_prepared = np.array(rgb_image.resize(TARGET_SIZE), dtype=np.uint8)
+img_prepared = img_prepared[np.newaxis, :, :]
 
-print(img_processed.shape)
+pred = model.predict(img_prepared)
+prediction = classes[np.argmax(pred, axis=1)]
+
+st.image(img, caption="Image")
+st.write(prediction)
