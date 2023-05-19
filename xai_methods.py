6669ebdefab62a372e626fdb95507588eb1d3f18
Dominik Hahn <dominik.hahn@hahnelektro.de>
2023-05-19 18:06:20 +0200

    Grad Cam finalized

--- xai_methods.py
+++ xai_methods.py
@@ -2,29 +2,28 @@ import cv2
 import numpy as np
 import tensorflow as tf
 import matplotlib.pyplot as plt
+import matplotlib.cm as cm
 
+from tensorflow import keras
 from keras import Model
 from keras.applications.vgg16 import preprocess_input
 from keras.models import load_model
 from keras.preprocessing import image
 
-IMG_DIR = "C:/Users/Dominik Hahn/Documents/GitHub/VisualAnalytics/data/images/VICE/xoKwbbnlxi0.jpg"
-model = load_model("xai_test.h5")
 
+## Grad CAM
 
-def preprocess_img(img_dir):
-    img = image.load_img(img_dir, target_size=(224, 224))
+def preprocess_img(img):
+    # img = image.load_img(img_dir, target_size=(224, 224))
     img = image.img_to_array(img)
     img = np.expand_dims(img, axis=0)
     return preprocess_input(img)
 
-
-def grad_cam(img_dir, model, layer_name):
+def grad_cam(img_array, model, layer_name):
     grad_model = Model(inputs=[model.inputs], outputs=[model.get_layer(layer_name).output, model.output])
 
     with tf.GradientTape() as tape:
-        img = preprocess_img(img_dir)
-        layer_output, predictions = grad_model(img)
+        layer_output, predictions = grad_model(img_array)
         loss = predictions[:, 0]
 
     gradients = tape.gradient(loss, layer_output)[0]
@@ -38,7 +37,7 @@ def grad_cam(img_dir, model, layer_name):
     weights = tf.reduce_mean(guided_gradients, axis=(0,1))
     grad_cam = tf.reduce_sum(tf.multiply(weights, layer_output), axis=-1)
 
-    width, height = img.shape[2], img.shape[1]
+    width, height = img_array.shape[2], img_array.shape[1]
     heatmap = cv2.resize(grad_cam.numpy(), (width, height))
     counter = heatmap - np.min(heatmap)
     denominator = (heatmap.max() - heatmap.min())
@@ -47,4 +46,22 @@ def grad_cam(img_dir, model, layer_name):
     
     return scaled_heatmap
 
-plt.imshow(grad_cam(IMG_DIR, model, "block5_conv3"))
+def display_grad_cam(img, heatmap):
+    img = keras.utils.img_to_array(img)
+    heatmap = np.uint8(255*heatmap)
+    
+    jet = cm.get_cmap("jet")
+    jet_colors = jet(np.arange(256))[:, :3]
+    jet_heatmap = jet_colors[heatmap]
+    jet_heatmap = keras.utils.array_to_img(jet_heatmap)
+    jet_heatmap = jet_heatmap.resize((img.shape[1], img.shape[0]))
+    jet_heatmap = keras.utils.img_to_array(jet_heatmap)
+
+    superimposed_img = jet_heatmap * 0.4 + img
+    superimposed_img = keras.utils.array_to_img(superimposed_img)
+    
+    return superimposed_img
+
+## Shapley Values
+
+## LRP
