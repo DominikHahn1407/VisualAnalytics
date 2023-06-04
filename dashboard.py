7585220020d9b1a24692c45ba2bef8f793320b41
Chriss280 <christoph.mattmann@studmail.htw-aalen.de>
2023-06-04 23:50:12 +0200

    dashboard

--- dashboard.py
+++ dashboard.py
@@ -33,11 +33,14 @@ with open(os.path.join(CSV_PATH, f"distribution_{version_selection}.csv"), "r") 
 tab1, tab2, tab3 = st.tabs(["Data", "Model", "Prediction"])
 
 with tab1:
-    selected_image = st.selectbox("Choose an specific Image", image_list)
+    col1, col2 = st.columns(2)
+    with col1:
+        selected_image = st.selectbox("Choose an specific Image", image_list)
+    
     IMG_PATH = os.path.join(IMAGE_PATH, selected_image)
     image = Image.open(os.path.join(IMG_PATH, os.listdir(IMG_PATH)[0]))
-    st.markdown(f"<h1>Image for Category: {selected_image}</h1>", unsafe_allow_html=True)
-    st.image(image)
+    # st.markdown(f"<h1>Image for Category: {selected_image}</h1>", unsafe_allow_html=True)
+    # st.image(image)
 
     distribution_list = [(key, value) for key, value in occurence_dict.items()]
     distribution_list.sort(key=lambda x: x[1], reverse=True)
@@ -49,9 +52,18 @@ with tab1:
     ax.set_xticklabels(categories, rotation=90)
     ax.set_xlabel("Categories")
     ax.set_ylabel("Occurences")
-    ax.set_title("Data Distribution")
-    st.pyplot(fig)
-    
+    # ax.set_title("Data Distribution")
+    # st.pyplot(fig)
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown(f"<h1 style='text-align: center;'>Image for Category: {selected_image}</h1>", unsafe_allow_html=True)
+        st.image(image)
+        st.markdown("<h1 style='text-align: center;'>Augmented Version</h1>", unsafe_allow_html=True)
+
+
+    with col2:
+        st.markdown("<h1 style='text-align: center;'>Data Distribution </h1>", unsafe_allow_html=True)
+        st.pyplot(fig) 
 
 
 with tab2:
@@ -70,6 +82,7 @@ with tab2:
     with tab2_col1_h1:
         st.write("Confusion Matrix")
         st.image(confusion_matrix, caption="Confusion Matrix")
+        st.markdown("<h1 style='text-align: center;'>Visualisierung des Models</h1>", unsafe_allow_html=True)
     # with tab2_col2_h1:
     #     st.write("Model Structure")
     #     st.image(structure, caption="Model Structure")
@@ -82,3 +95,23 @@ with tab2:
     with tab2_col2_h2:
         st.write("Training Loss")
         st.image(train_loss, caption="Training Loss")
+
+with tab3:
+    col1, col2 = st.columns(2)
+    with col1:
+        st.markdown("<h1 style='text-align: center;'>Original Bild</h1>", unsafe_allow_html=True)
+        
+
+    with col2:
+        st.markdown("<h1 style='text-align: center;'>Barchart Prediction (Confidences)</h1>", unsafe_allow_html=True)
+
+
+    col1, col2, col3 = st.columns(3)
+    with col1:
+        st.markdown("<h1 style='text-align: center;'>Grad CAM</h1>", unsafe_allow_html=True)
+    with col2:
+        st.markdown("<h1 style='text-align: center;'>Lime</h1>", unsafe_allow_html=True)
+    with col3:
+        st.markdown("<h1 style='text-align: center;'>Shep</h1>", unsafe_allow_html=True)
+
+    
